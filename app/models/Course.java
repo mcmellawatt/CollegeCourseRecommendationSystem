503844d3bb5adb55413b2db6b91323c311601453
Simon Hunt <simon@onlab.us>
2015-11-12 23:55:38 -0800

    Augmented course data and updated web page listing of courses.

--- app/models/Course.java
+++ app/models/Course.java
@@ -1,13 +1,13 @@
 package models;
 
-import java.util.*;
-import javax.persistence.*;
+import play.data.format.Formats;
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
 
-import play.db.ebean.*;
-import play.data.format.*;
-import play.data.validation.*;
-
-import com.avaje.ebean.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
 
 /**
  * Course entity managed by Ebean
@@ -22,30 +22,44 @@ public class Course extends Model {
     public String id;
 
     @Constraints.Required
+    public String tag;
+
+    @Constraints.Required
     public String name;
+
+    @Constraints.Required
+    public String abbrev;
+
+    @Constraints.Required
+    public boolean core;
 
     // -- Queries
 
-    public static Model.Finder<String, Course> find = new Model.Finder(String.class, Course.class);
+    private static final Model.Finder<String, Course> FIND =
+            new Model.Finder<>(String.class, Course.class);
 
     /**
      * Retrieve all users.
      */
     public static List<Course> findAll() {
-        return find.all();
+        return FIND.all();
     }
 
     /**
      * Retrieve a Course from id.
      */
     public static Course findById(String id) {
-        return find.where().eq("id", id).findUnique();
+        return FIND.where().eq("id", id).findUnique();
     }
 
     // --
 
     public String toString() {
-        return "Course(" + id + ")";
+        return "Course{" + tag + "}";
+    }
+
+    public String title() {
+        return tag + " " + name;
     }
 
 }
